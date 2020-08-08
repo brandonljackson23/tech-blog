ad5100f35b2a75170599e5900f40d18811e7ecf5
Brandon Jackson <brandonljackson23@gmail.com>
2020-08-08 17:57:53 -0600

    fixed server connection

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,31 +4,32 @@ const hash = bcrypt.hashSync(password, 10);
 
 const { User } = require('../models');
 
-const userData = [{
-    username: 'TJ',
-    email: 'shorty22@gmail.com',
-    password: hash,
-},
-{
-    username: 'Casey',
-    email: 'givememore7@gmail.com',
-    password: hash,
-},
-{
-    username: 'Brock',
-    email: 'neverkissedagirl40@gmail.com',
-    password: hash,
-},
-{
-    username: 'Clint',
-    email: 'likeclockwork2pm@gmail.com',
-    password: hash,
-},
-{
-    username: 'Julie',
-    email: 'juliebean32@gmail.com',
-    password: hash,
-},
+const userData = [
+    {
+        username: 'TJ',
+        email: 'shorty22@gmail.com',
+        password: hash,
+    },
+    {
+        username: 'Casey',
+        email: 'givememore7@gmail.com',
+        password: hash,
+    },
+    {
+        username: 'Brock',
+        email: 'neverkissedagirl40@gmail.com',
+        password: hash,
+    },
+    {
+        username: 'Clint',
+        email: 'likeclockwork2pm@gmail.com',
+        password: hash,
+    },
+    {
+        username: 'Julie',
+        email: 'juliebean32@gmail.com',
+        password: hash,
+    },
 ];
 
 const seedUser = () => User.bulkCreate(userData);
