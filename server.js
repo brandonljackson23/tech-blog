6ec7ccacbb9879a7246a449ac65e5224d9fe7091
Brandon Jackson <brandonljackson23@gmail.com>
2020-08-09 07:53:05 -0600

    added javascript, helpers and views

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3006;
@@ -23,8 +23,8 @@ const sess = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(session(sess));
 
 // turn on routes
